6614b2e90f4f94d04de3472eb6d0a8fd045e275b
ser-rum <serj1988rum@gmail.com>
2022-04-28 12:26:27 +0300

    New class of tests with authorization and setup fixture.

--- pages/product_page.py
+++ pages/product_page.py
@@ -5,7 +5,6 @@ class ProductPage(BasePage):
 
     def add_to_the_cart(self):
         self.browser.find_element(*ProductPageLocators.ADD_TO_CART_BUTTON).click()
-        self.solve_quiz_and_get_code()
 
     def should_be_right_name(self, name):
         assert name == self.browser.find_element(*ProductPageLocators.ITEM_IN_CART_NAME).text, "The name doesn`t match"
